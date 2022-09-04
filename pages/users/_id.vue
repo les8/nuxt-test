c1ec9fb0a05bd3a0a6c22f9ea3fa1e46a34fe7f6
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-09-04 12:52:11 +0300

    feat: dymanic routes

---
+++ pages/users/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>User {{ id }}</h1>
+    <NuxtLink to="/">to hone page</NuxtLink>
+    <NuxtLink to="/users">to users page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Users</h1>
     <NuxtLink to="/users/create">to create users page</NuxtLink>
+    <NuxtLink to="/users/1">to user 1</NuxtLink>
   </div>
 </template>
 
